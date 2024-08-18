a3e0c60c5d569dca0ef4af4e9547c0cab0ed553a
mick shannahan <beasticorn@gmail.com>
2024-08-18 06:58:12 -0600

    bang updated

--- Bang/testSuite.js
+++ Bang/testSuite.js
@@ -11,9 +11,26 @@ export const hints = [
 function challenge(gun1, gun2) {
   const speed1 = gun1.indexOf('G')
   const speed2 = gun2.indexOf('G')
-  if(speed1 < speed2) return 1
+  if (speed1 < speed2) return 1
   return 2
 }
 
 // export out the function to be visible in the console
 export const solution = challenge.toString()
+
+
+
+// here is where you can write the multiple test cases for the challenge
+let showdown1 = ['___BANG___', '______BANG']
+let showdown2 = ['__BANG', 'BANG__']
+let showdown3 = ['B___AN____G_', '___B_A_NG___']
+
+export function testSuite(test, challenge) {
+  //format follows: challange(<actual input sent to the students function>), expected output, given input)
+  // try to come up with atleast 5
+  //for more challenge try to mix up data types
+  test(challenge(...showdown1), 1, showdown1.join(' '))
+  test(challenge(...showdown2), 2, showdown2.join(' '))
+  test(challenge(...showdown3), 2, showdown3.join(' '))
+
+}
