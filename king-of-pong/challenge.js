52320bac100f527bb98b3e280cd84310759154c8
Mick <beasticorn@gmail.com>
2024-10-07 08:52:30 -0600

    whoops forgot to erase solution

--- king-of-pong/challenge.js
+++ king-of-pong/challenge.js
@@ -16,28 +16,12 @@
 const kopUrl = 'https://sandbox.codeworksacademy.com/api/kop/match-history'
 
 async function kingOfPong() {
-  const response = await fetch(kopUrl)
-  const data = await response.json()
-  console.log('data', data)
-  let winner = data[0]
-  data.forEach(player => {
-    if (winner.wins <= player.wins) {
-      winner = player
-    }
-  })
-  return winner.playerName
+
+
 }
 
 
 async function kingOfVirtue() {
-  const response = await fetch(kopUrl)
-  const data = await response.json()
-  let virtuePlayer = data[0]
-  data.forEach(player => {
-    if (player.penalties > 0) return
-    if (virtuePlayer.wins <= player.wins) {
-      virtuePlayer = player
-    }
-  })
-  return virtuePlayer.playerName
+
+
 }
