31f0ffe6c5aba79b6b534ec15e60640670d30975
Mick <beasticorn@gmail.com>
2025-02-25 14:28:33 -0700

    adding a challenge for debugging

---
+++ Debugging1/1/challenge.js
@@ -0,0 +1,8 @@
+
+
+// -- Update the function below as needed. 
+// -- Do not change the name of the function
+function evenOrOdd(num) {
+
+
+}
---
+++ Debugging1/1/testSuite.js
@@ -0,0 +1,21 @@
+/**
+ * @callback testCallback
+ * @param {function} function - challenge function to run.
+ * @param {*} expected - expected output.
+ * @param {...*} arguments - arguments passed to function.
+ */
+
+/**
+ * @param {testCallback} test 
+ */
+async function* testSuite(test) {
+
+  yield await test(evenOrOdd, 'even', 2)
+  yield await test(evenOrOdd, 'odd', 3)
+  yield await test(evenOrOdd, 'even', 24)
+  yield await test(evenOrOdd, 'odd', -151)
+  yield await test(evenOrOdd, 'even', 0)
+
+  yield 'TEST:END'
+
+}
