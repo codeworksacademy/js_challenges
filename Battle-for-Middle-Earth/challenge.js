a31862d284f85373d40f5d46ec0922d1611381ab
Jake Overall <joverall22@gmail.com>
2025-07-24 12:32:15 -0600

    Update challenge.js

--- Battle-for-Middle-Earth/challenge.js
+++ Battle-for-Middle-Earth/challenge.js
@@ -3,20 +3,3 @@
 function challenge(forces) {
 
 }
-
-// Uncomment next line to show inputs in console
-// @ts-ignore
-window.showInput = true
-
-// Uncomment next line to disable Hints
-// @ts-ignore
-// window.disableHints = true
-
-// Uncomment next line to show solution
-// @ts-ignore
-// window.showSolution = true
-
-
-// -- Do not change the name of the function
-//@ts-ignore
-window.challenge = challenge
